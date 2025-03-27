36b81995c320da59d5c3e2c3412793fbb8d0126d
Bytomancer <bytomancer@gmail.com>
2025-03-26 22:37:17 -0400

    Missing cargo-watch

--- flake.nix
+++ flake.nix
@@ -20,6 +20,7 @@
       devShells.${system}.default = pkgs.mkShell {
         packages = with pkgs; [
           rust
+          cargo-watch
           gnumake
           docker
           dbeaver-bin
