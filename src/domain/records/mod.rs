9a8b525d06ae3265262f77e593326629f49ae128
Bytomancer <bytomancer@gmail.com>
2025-10-15 19:18:01 -0400

    Record refactor

--- src/domain/records/mod.rs
+++ src/domain/records/mod.rs
@@ -1,24 +1,5 @@
-use crate::domain::domain_error::DomainResult;
-#[allow(unused_imports)]
-use crate::prelude::*;
-
 pub mod creature;
+pub mod record;
 pub mod session;
 
-#[allow(dead_code)]
-pub trait Record: Sized {
-    async fn find_by_id(
-        conn: &sqlx::PgPool,
-        id: &uuid::Uuid,
-    ) -> DomainResult<Self>;
-
-    async fn save(
-        &self,
-        conn: &sqlx::PgPool,
-    ) -> DomainResult<()>;
-
-    async fn delete(
-        self,
-        conn: &sqlx::PgPool,
-    ) -> DomainResult<()>;
-}
+pub use record::Record;
