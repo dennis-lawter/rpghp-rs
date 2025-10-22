37231f1c3022fc705cab8a2d76525a3311942213
Bytomancer <bytomancer@gmail.com>
2025-10-21 22:28:21 -0400

    Moved shared state impl to module's core

---
+++ src/server/shared_state/core.rs
@@ -0,0 +1,36 @@
+use handlebars::Handlebars;
+use poem::IntoResponse;
+use poem::http::StatusCode;
+use poem_openapi::payload;
+
+use crate::Config;
+use crate::domain::Domain;
+use crate::prelude::*;
+
+#[derive(Clone)]
+pub struct SharedState {
+    pub domain: Domain,
+    pub hb: Handlebars<'static>,
+}
+impl SharedState {
+    pub async fn new(cfg: &Config) -> CrateResult<Self> {
+        let domain = Domain::new(cfg).await?;
+
+        let mut hb = Handlebars::new();
+        super::template_registry::register_hbs_files_from_dir(&mut hb, "./handlebars")?;
+
+        Ok(Self { domain, hb })
+    }
+
+    pub fn render(
+        &self,
+        id: &str,
+        json: serde_json::Value,
+    ) -> poem::Result<impl IntoResponse> {
+        let html = self.hb.render(id, &json).map_err(|e| {
+            poem::Error::from_string(e.to_string(), StatusCode::INTERNAL_SERVER_ERROR)
+        })?;
+
+        Ok(payload::Html(html))
+    }
+}
--- src/server/shared_state/mod.rs
+++ src/server/shared_state/mod.rs
@@ -1,38 +1,4 @@
+pub mod core;
 mod template_registry;
 
-use handlebars::Handlebars;
-use poem::IntoResponse;
-use poem::http::StatusCode;
-use poem_openapi::payload;
-
-use crate::Config;
-use crate::domain::Domain;
-use crate::prelude::*;
-
-#[derive(Clone)]
-pub struct SharedState {
-    pub domain: Domain,
-    pub hb: Handlebars<'static>,
-}
-impl SharedState {
-    pub async fn new(cfg: &Config) -> CrateResult<Self> {
-        let domain = Domain::new(cfg).await?;
-
-        let mut hb = Handlebars::new();
-        template_registry::register_hbs_files_from_dir(&mut hb, "./handlebars")?;
-
-        Ok(Self { domain, hb })
-    }
-
-    pub fn render(
-        &self,
-        id: &str,
-        json: serde_json::Value,
-    ) -> poem::Result<impl IntoResponse> {
-        let html = self.hb.render(id, &json).map_err(|e| {
-            poem::Error::from_string(e.to_string(), StatusCode::INTERNAL_SERVER_ERROR)
-        })?;
-
-        Ok(payload::Html(html))
-    }
-}
+pub use core::SharedState;
